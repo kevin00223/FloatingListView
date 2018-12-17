7a4e7c598f3bc580320ae7ceef7e2bdf5a2c3628
李凯 <likai.new@gmail.com>
2018-12-17 14:00:53 +0800

    调整floatListView布局

--- FloatingListView/FloatingListView/FloatView/LKFloatListView.m
+++ FloatingListView/FloatingListView/FloatView/LKFloatListView.m
@@ -13,10 +13,15 @@
 #define floatListItemWidth 100  //不写固定值
 #define floatListItemHeight 40
 
-@interface LKFloatListItem ()
+@interface LKFloatListView ()
 
-//@property (nonatomic, strong) UIView *containerView;
-//@property (nonatomic, strong) UIView *singleLine;
+@property (nonatomic, strong) UIView *containerView;
+@property (nonatomic, strong) NSMutableArray *buttons;
+@property (nonatomic, strong) NSMutableArray *lines;
+
+@property (nonatomic, strong) UIColor *floatViewBackgroundColor;
+@property (nonatomic, strong) UIColor *textColor;
+@property (nonatomic, strong) UIColor *lineColor;
 
 @end
 
@@ -25,46 +30,95 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor clearColor];
+        [self initValues];
+        [self initSubviews];
     }
     return self;
 }
 
-- (void)setItems:(NSArray<LKFloatListItem *> *)items {
-    _items = items;
-    [self initSubviews];
+- (void)initValues {
+    self.backgroundColor = [UIColor clearColor];
+    self.buttons = [NSMutableArray array];
+    self.lines = [NSMutableArray array];
+    self.floatViewBackgroundColor = [UIColor blackColor];
+    self.textColor = [UIColor whiteColor];
+    self.lineColor = [UIColor whiteColor];
 }
 
 - (void)initSubviews {
-    UIView *containerView = [[UIView alloc]init];
-    [self addSubview:containerView];
-    containerView.backgroundColor = [UIColor blackColor];
-    [containerView mas_makeConstraints:^(MASConstraintMaker *make) {
+    self.containerView = [[UIView alloc]init];
+    self.containerView.backgroundColor = self.floatViewBackgroundColor;
+    self.containerView.layer.cornerRadius = 4.0;
+    self.containerView.layer.masksToBounds = YES;
+    [self addSubview:self.containerView];
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    [self.containerView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self).offset(kStatusAndNavigationBarHeight + 6);
         make.right.equalTo(self).offset(-10);
-        make.width.offset(floatListItemWidth);
+        make.width.offset(floatListItemWidth+20);
         make.height.offset(self.items.count * floatListItemHeight);
     }];
     
+    int i = 0;
+    for (UIButton *button in self.buttons) {
+        [button setFrame:CGRectMake(10, i * floatListItemHeight, floatListItemWidth, floatListItemHeight)];
+        i ++;
+    }
+    
+    i = 0;
+    for (UIView *singleLine in self.lines) {
+        [singleLine setFrame:CGRectMake(10, (i+1)*floatListItemHeight, floatListItemWidth, 1/[UIScreen mainScreen].scale)];
+    }
+}
+
+- (void)setItems:(NSArray<LKFloatListItem *> *)items {
+    _items = items;
+    
+    [self resetSubviews];
+    [self setNeedsLayout];
+}
+
+//- (void)setFloatViewBackgroundColor:(UIColor *)floatViewBackgroundColor {
+//    _floatViewBackgroundColor = floatViewBackgroundColor;
+//    self.containerView.backgroundColor = self.floatViewBackgroundColor;
+//}
+
+//- (void)setTextColor:(UIColor *)textColor {
+//    _textColor = textColor;
+//    for (UIButton *btn in self.buttons) {
+//        [btn setTitleColor:self.textColor forState:UIControlStateNormal];
+//    }
+//}
+
+//- (void)setLineColor:(UIColor *)lineColor {
+//    _lineColor = lineColor;
+//    for (UIView *line in self.lines) {
+//        line.backgroundColor = self.lineColor;
+//    }
+//}
+
+- (void)resetSubviews {
     int i = 0;
     for (LKFloatListItem *item in self.items) {
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
         [btn setImage:[UIImage imageNamed:item.imageName] forState:UIControlStateNormal];
         [btn setTitle:item.text forState:UIControlStateNormal];
-        [btn setFrame:CGRectMake(0, i * floatListItemHeight, floatListItemWidth, floatListItemHeight)];
-        [containerView addSubview:btn];
+        [btn setTitleColor:self.textColor forState:UIControlStateNormal];
+        [self.containerView addSubview:btn];
+        [self.buttons addObject:btn];
         
         if (self.items.lastObject != item) {
             UIView *singleLine = [[UIView alloc]init];
-            singleLine.backgroundColor = [UIColor whiteColor];
-            [containerView addSubview:singleLine];
-            [singleLine mas_makeConstraints:^(MASConstraintMaker *make) {
-                make.left.right.bottom.equalTo(btn);
-                make.height.offset(1/[UIScreen mainScreen].scale);
-            }];
+            singleLine.backgroundColor = self.lineColor;
+            [self.containerView addSubview:singleLine];
+            [self.lines addObject:singleLine];
         }
         
-        i++;
+        i ++;
     }
 }
 
